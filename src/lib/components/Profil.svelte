9aa07e511cc5ec8f883c6e55f0e6335ea6a88e62
root <thomas.degand@outlook.fr>
2023-10-09 17:43:56 +0200

    First Page

---
+++ src/lib/components/Profil.svelte
@@ -0,0 +1,77 @@
+<!---- PROFIL ------->
+
+
+<!------------------->
+<!---- CODE JS ------>
+<!------------------->
+<script>
+    export let top = 25;
+    export let right = 100/(6*2); //en vw
+    export let height = 60; //en vh
+	export let width = 5*100/(6*2); //en vw
+    export let zMin = 1;
+    export let title = "";
+    export let cTitle = "#FFFFFF";
+    export let pTitle = 0.15;
+    export let subtitle = "";
+    export let cSubtitle = "#FFFFFF";
+    export let pSubtitle = 0.13;
+    export let description = "";
+    export let cDescription = "#FFFFFF";
+    export let pDescription = 0.72;
+    export let nLines = 10;
+
+    function I(name) { return document.getElementById(name); }
+	
+</script>
+
+
+<!------------------->
+<!---- CODE HTML ---->
+<!------------------->
+<div class="boxProfil" style="top:{top}vh;right:{right}vw;z-index:{zMin};height:{height}vh;width:{width}vw;">
+    <div class="titleProfil" style="height:{pTitle*height}vh;width:{width}vw;line-height:{pTitle*height}vh;font-size:{0.9*pTitle*height}vh;color:{cTitle};">{title}</div>
+    <div class="subtitleProfil" style="top:{pTitle*height}vh;height:{pSubtitle*height}vh;width:{width}vw;line-height:{pTitle*height/2}vh;font-size:{0.9*pSubtitle*height/2}vh;color:{cSubtitle};">{subtitle}</div>
+    <div class="descriptionProfil" style="top:{(pTitle+pSubtitle)*height}vh;height:{pDescription*height}vh;width:{width}vw;line-height:{pDescription*height/nLines}vh;font-size:{0.9*pDescription*height/nLines}vh;color:{cDescription};">{description}</div>
+</div>
+
+<!------------------->
+<!---- CODE CSS ----->
+<!------------------->
+<style>
+.boxProfil
+{
+    margin: 0;
+    padding: 0;
+    position: absolute;
+    font-family: 'Roboto';
+}
+.titleProfil
+{
+    margin: 0;
+    padding: 0;
+    position: absolute;
+    top: 0;
+    left: 0;
+    text-align: left;
+    font-weight: 500;
+}
+.subtitleProfil
+{
+    margin: 0;
+    padding: 0;
+    position: absolute;
+    left: 0;
+    text-align: left;
+    font-weight: 300;
+}
+.descriptionProfil
+{
+    margin: 0;
+    padding: 0;
+    position: absolute;
+    left: 0;
+    text-align: justify;
+    font-weight: 400;
+}
+</style>
---
+++ transfer/lib/components/Avatar.svelte
@@ -0,0 +1,52 @@
+<!---- AVATAR ------->
+
+
+<!------------------->
+<!---- CODE JS ------>
+<!------------------->
+<script>
+    export let top = 50;
+    export let left = 25 - 100/(5*2);
+	export let width = 100/5; //en vw
+    export let zMin = 2;
+    export let cBack = "#111111";
+    export let cBorder = "#FFFFFF";
+    export let wBorder = 0.4;
+    export let src = "https://data.aerles.fr/svelte/avatar.png";
+    export let x = 1;
+
+    function I(name) { return document.getElementById(name); }
+	
+</script>
+
+
+<!------------------->
+<!---- CODE HTML ---->
+<!------------------->
+<div class="frameAvatar" style="top:calc({top}vh - {width/2}vw);left:{left}vw;z-index:{zMin};height:{width}vw;width:{width}vw;background:{cBack};border:solid {wBorder}vw {cBorder};box-shadow:inset 0 0 {2*wBorder}vw {cBorder};">
+    <div class="photoAvatar" style="top:{x*2*wBorder}vw;left:{x*2*wBorder}vw;z-index:{zMin+1};height:{width - (2+ x*4)*wBorder}vw;width:{width - (2+ x*4)*wBorder}vw;background:url('{src}'),{cBorder};border:solid {wBorder}vw {cBorder};">
+
+    </div>
+</div>
+
+<!------------------->
+<!---- CODE CSS ----->
+<!------------------->
+<style>
+.frameAvatar
+{
+    margin: 0;
+    padding: 0;
+    position: absolute;
+    box-sizing: border-box;
+    transform: rotate(-45deg);
+}
+.photoAvatar
+{
+    margin: 0;
+    padding: 0;
+    position: absolute;
+    box-sizing: border-box;
+    background-size: cover !important;
+}
+</style>
